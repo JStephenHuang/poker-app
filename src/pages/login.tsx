631cb2463b8c0de87ac9756588d53bf83270b225
JStephenHuang <jstephhuang@gmail.com>
2023-01-05 23:40:42 -0500

    Login Page

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -3,7 +3,6 @@ import Navbar from "../components/navbar/navbar";
 const LoginPage = () => {
   return (
     <div className="w-screen h-screen">
-      <Navbar />
       <div className="flex flex-col items-center">
         <p className="text-[30px] my-5">Login page</p>
       </div>
